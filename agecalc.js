76739ac65d0c8127bc436c6cb28d1539bf84b0b2
greenhalghdan <49040634+greenhalghdan@users.noreply.github.com>
2020-09-29 12:32:39 +0100

    Add files via upload

---
+++ agecalc.js
@@ -0,0 +1,4 @@
+var age = prompt("How old are you?");
+var daysBeenAlive = age * 365.25;
+console.log("You have been alive for " + daysBeenAlive + " days");
+alert("You have been alive for " + daysBeenAlive + " days");
